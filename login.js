65c632fe904379c90286f4c198055558c1a7d5ec
yum.co <101381228+yumco@users.noreply.github.com>
2023-06-12 16:30:49 +0900

    2023/06/12
    로그인 submit 마우스 클릭 이벤트
    감지 설정함

---
+++ login.js
@@ -0,0 +1,12 @@
+const loginFrom = document.querySelector("#login_form")
+
+
+
+
+function BtnClick() {
+   
+    console.log("click!!!")
+    return false;
+}
+
+loginFrom.addEventListener("click", BtnClick);
